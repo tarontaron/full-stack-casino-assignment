28f2c9ea033152f4544a6c55b7f0dab0a62c4903
Taron Alaverdyan <taronalaverdyan@Tarons-MacBook-Pro.local>
2025-09-04 01:33:11 +0400

    feat(db): add seed script

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,43 @@
+import { PrismaClient, Role } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+const users = [
+  {
+    email: 'testbot6654@gmail.com',
+    password: 'Password1*',
+    first_name: 'Test',
+    last_name: 'Player',
+    role: Role.PLAYER,
+  },
+];
+
+const main = async () => {
+  try {
+    // Clear existing users
+    await prisma.user.deleteMany();
+
+    for (const user of users) {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+
+      await prisma.user.create({
+        data: {
+          email: user.email,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          role: user.role,
+          password_hash: hashedPassword,
+        },
+      });
+    }
+
+    console.log('Database has been seeded with multiple users.');
+  } catch (error) {
+    console.error('Error while seeding the database:\n', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+main();
